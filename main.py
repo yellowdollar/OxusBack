c7154c3c5b79537920f4b0de1ae17004b2aeab14
yellowdollar <yellowdollar55@gmail.com>
2025-01-29 03:34:02 +0500

    PUSH

--- main.py
+++ main.py
@@ -21,9 +21,6 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(
     lifespan = lifespan,
-    root_path='/api',
-    openapi_url='/openapi.json',
-    docs_url='/docs',
 )
 app.include_router(router = news_router)
 app.include_router(router = users_router)
