e9cf00cb3a535ced80183022970492c4b4b7960b
yellowdollar <yellowdollar55@gmail.com>
2025-02-14 15:23:48 +0500

    push

--- api/regist.py
+++ api/regist.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
-from fastapi import Depends, Form, status
+from fastapi import APIRouter, Depends, Form, status
+import aiosmtplib
+from email.message import EmailMessage
 
 from api.dependencies import users_service
 from services.users import UserService
@@ -9,11 +10,42 @@ from services.regist import RegistService
 
 from typing import Annotated
 
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+SMTP_USERNAME = "yellowdollar55@gmail.com"
+SMTP_PASSWORD = "klot klsw tkaa iput"
+
+RECIPIENT_EMAIL = "jewelrysanoattj@gmail.com"  # Куда отправлять уведомление
 
 regist_router = APIRouter(
-    prefix = '/regist',
-    tags = ['Regist Routes']
+    prefix='/regist',
+    tags=['Regist Routes']
 )
+
+async def send_email_notification(name: str, surname: str, email: str, phone: str, smth: str):
+    """Функция отправки email при регистрации"""
+    msg = EmailMessage()
+    msg["From"] = SMTP_USERNAME
+    msg["To"] = RECIPIENT_EMAIL
+    msg["Subject"] = "Новый пользователь отправил запрос на регистрацию"
+
+    msg.set_content(
+        f"Новая регистрация:\n\n"
+        f"Имя: {name}\n"
+        f"Фамилия: {surname}\n"
+        f"Email: {email}\n"
+        f"Телефон: {phone}\n"
+        f"Дополнительно: {smth}"
+    )
+
+    await aiosmtplib.send(
+        msg,
+        hostname=SMTP_SERVER,
+        port=SMTP_PORT,
+        start_tls=True,
+        username=SMTP_USERNAME,
+        password=SMTP_PASSWORD
+    )
 
 @regist_router.post('/add_regist')
 async def add_regist(
@@ -32,7 +64,10 @@ async def add_regist(
         'smth': smth
     }
 
-    result = await regist_service.add_new_regist(data = data)
+    result = await regist_service.add_new_regist(data=data)
+
+    # Отправляем email
+    await send_email_notification(name, surname, email, phone, smth)
 
     return {
         'status_code': status.HTTP_200_OK,
